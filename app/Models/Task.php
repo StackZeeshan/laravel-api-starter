3b4e9e5c6d336cc1afd8f14766c96679f3b27a15
Ali Zeeshan <umarabass.dev@gmail.com>
2026-01-06 11:48:03 +0500

    Create Task model with migration and user relationship

---
+++ app/Models/Task.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'status',
+        'due_date',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'due_date' => 'datetime',
+    ];
+
+    /**
+     * Relationship: Task belongs to a User.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
